00c9985b4428ff8d5268a03a9ab75c12ce19326d
Keerthana Devi Govindaraj <govindaraj.ke@northeastern.edu>
2025-11-06 19:52:06 -0500

    Modified upload.js

--- Assignment8/middleware/upload.js
+++ Assignment8/middleware/upload.js
@@ -11,6 +11,7 @@ const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'images/');
     },
+    
     filename: function (req, file, cb) {
         try {
             const email = req.body.email;
