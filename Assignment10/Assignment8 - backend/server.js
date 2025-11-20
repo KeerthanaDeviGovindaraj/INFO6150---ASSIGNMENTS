eeb86618995eac6c51ea61d08ab6ada29c19f309
Keerthana Devi Govindaraj <govindaraj.ke@northeastern.edu>
2025-11-20 13:45:59 -0500

    modified app.use

--- Assignment10/Assignment8 - backend/server.js
+++ Assignment10/Assignment8 - backend/server.js
@@ -6,41 +6,33 @@ const fs = require('fs');
 const cors = require('cors');
 const { swaggerUi, specs } = require('./swagger');
 
-// Load environment variables
 dotenv.config();
 
-// Initialize Express app
 const app = express();
 
-// CORS configuration
 app.use(cors({
   origin: 'http://localhost:3001',
   credentials: true
 }));
 
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Create images directory if it doesn't exist
 const imagesDir = path.join(__dirname, 'images');
 if (!fs.existsSync(imagesDir)) {
     fs.mkdirSync(imagesDir);
     console.log('Images directory created');
 }
 
-// Serve static files
 app.use('/images', express.static(imagesDir));
 
-// Import routes
+
 const userRoutes = require('./routes/userRoutes');
 const jobRoutes = require('./routes/jobRoutes');
 
-// Use routes with /api prefix
 app.use('/api', userRoutes);
 app.use('/api', jobRoutes);
 
-// MongoDB connection
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
@@ -53,10 +45,10 @@ const connectDB = async () => {
     }
 };
 
-// Connect to database
+
 connectDB();
 
-// Mongoose connection events
+
 mongoose.connection.on('connected', () => {
     console.log('Mongoose connected to MongoDB');
 });
@@ -69,20 +61,20 @@ mongoose.connection.on('disconnected', () => {
     console.log('Mongoose disconnected from MongoDB');
 });
 
-// Graceful shutdown
+
 process.on('SIGINT', async () => {
     await mongoose.connection.close();
     console.log('MongoDB connection closed due to app termination');
     process.exit(0);
 });
 
-// Swagger documentation
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs, {
     customCss: '.swagger-ui .topbar { display: none }',
     customSiteTitle: 'User Management API Docs'
 }));
 
-// Root route
+
 app.get('/', (req, res) => {
     res.json({ 
         message: 'User API Server is running',
@@ -97,7 +89,6 @@ app.get('/', (req, res) => {
     });
 });
 
-// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
